ff8ffe8b6dcf75d91f613aa3280f30e821375714
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2017-05-02 13:21:49 +0900

    DBtest

---
+++ database/migrations/2017_05_02_121730_create_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('post_id');
+            $table->timestamps();
+            $table->string('comment',120);
+            $table->integer('id');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post');
+    }
+}
