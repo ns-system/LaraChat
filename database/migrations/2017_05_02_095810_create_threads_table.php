151e5bd2ae449bbc097c02bafb0b2f8da8578d66
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2017-05-02 13:23:57 +0900

    DBtest

--- database/migrations/2017_05_02_095810_create_threads_table.php
+++ database/migrations/2017_05_02_095810_create_threads_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateThreadsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -11,14 +11,12 @@ class CreateUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+    {        
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('threads_id');
+            $table->integer('post_id');
             $table->timestamps();
+            $table->string('creator',30);
         });
     }
 
@@ -29,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('threads');
     }
 }
--- database/migrations/2017_05_02_131103_create_threadsparticipant_table.php
+++ database/migrations/2017_05_02_131103_create_threadsparticipant_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateThreadsparticipantTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+        Schema::create('threadsparticipant', function (Blueprint $table) {
+            $table->increments('threads_participant_id');
             $table->timestamps();
+            $table->integer('post_id');
+            $table->integer('threads_id');
+            
         });
     }
 
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('threadsparticipant');
     }
 }
